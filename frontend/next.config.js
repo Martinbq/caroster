9e34e8f8a81531cca5577a033498f0a3a5359325
Martinbq <114339264+Martinbq@users.noreply.github.com>
2025-10-21 23:58:59 +0300

    Refactor Next.js configuration for standalone output

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,58 +1,13 @@
-const {i18n} = require('./next-i18next.config');
-
-const {
-  NODE_ENV,
-  DEV_TILES_URL,
-  STRAPI_URL = 'http://localhost:1337',
-  FALLBACK_LANGUAGE = 'en',
-  MAPBOX_TOKEN,
-  MAPBOX_URL,
-} = process.env;
-
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: NODE_ENV !== 'production',
-});
-
-module.exports = withPWA({
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    // This is required for standalone output
+    outputFileTracingRoot: process.cwd(),
+  },
+  output: 'standalone',
+  // Add other configurations as needed
+  reactStrictMode: true,
   swcMinify: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  env: {
-    STRAPI_URL,
-    DEV_TILES_URL,
-    FALLBACK_LANGUAGE,
-    MAPBOX_CONFIGURED: !!MAPBOX_TOKEN && !!MAPBOX_URL,
-  },
-  i18n,
+}
 
-  trailingSlash: true,
-  async rewrites() {
-    return [
-      {
-        source: '/graphql',
-        destination: `${STRAPI_URL}/graphql`,
-      },
-      {
-        source: '/api/nauth/:slug*',
-        destination: `/api/nauth/:slug*`,
-      },
-      {
-        source: '/admin/:slug*',
-        destination: `${STRAPI_URL}/admin/:slug*`,
-      },
-      {
-        source: '/i18n/:slug*',
-        destination: `${STRAPI_URL}/i18n/:slug*`,
-      },
-      {
-        source: '/content-manager/:slug*',
-        destination: `${STRAPI_URL}/content-manager/:slug*`,
-      },
-    ];
-  },
-});
+module.exports = nextConfig
